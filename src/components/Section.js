9e106cf74c29632ce4815d63d7e29a3c3afadaa0
Apdet <deterding1.a@gmail.com>
2021-05-16 22:12:44 -0500

    added homepage image with text and above the image and buttons below the image, used flexbox styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,19 @@ import styled from 'styled-components';
 const Section = () => {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online For Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -14,6 +26,39 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 
 `;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+`;
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+
+`;
+
+const RightButton = styled(LeftButton)``;
