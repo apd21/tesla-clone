301f3036c018e95c31a66514a80b1fa3b37905b8
Apdet <deterding1.a@gmail.com>
2021-05-20 16:15:31 -0500

    added props with additional sections in section.js to display additional images with text

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const Section = () => {
+const Section = ({ title, description, rightBtnText, leftBtnText, backgroundImg }) => {
     return (
-        <Wrap>
+        <Wrap bgImage={backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online For Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
-            <ButtonGroup>
-                <LeftButton>
-                    Custom Order
-                </LeftButton>
-
-                <RightButton>
-                    Existing Inventory
-                </RightButton>
-            </ButtonGroup>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+                    { rightBtnText &&  
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>
+                    }
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -29,12 +33,11 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
+    background-image: ${props => `url("/images/${props.bgImage}")` };
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-
 `;
 
 const ItemText = styled.div`
@@ -44,6 +47,10 @@ const ItemText = styled.div`
 
 const ButtonGroup = styled.div`
     display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 
 const LeftButton = styled.div`
@@ -58,7 +65,21 @@ const LeftButton = styled.div`
     opacity: 0.85;
     text-transform: uppercase;
     font-size: 12px;
-
+    cursor: pointer;
+    margin: 8px;
 `;
 
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.65;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div``;
