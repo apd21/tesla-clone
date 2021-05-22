be41e3026e1fca0af587369961c98c4815f42040
Apdet <deterding1.a@gmail.com>
2021-05-22 17:34:42 -0500

    created sidebar menu with menu icon, added car list to sidebar with redux, deleted unnecessary redux starter files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
-import Fade from 'react-reveal/Fade';
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
+
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false);
+    const cars = useSelector(selectCars);
     return (
         <Container>
           <a>
             <img src="/images/logo.svg" alt="Tesla Logo" />
           </a> 
           <Menu>
-            <a href="#">Model S</a>
-            <a href="#">Model Y</a>
-            <a href="#">Model 3</a>
-            <a href="#">Model X</a>
+              { cars && cars.map((car, index) => (
+                <a key={index} href="#">{car}</a>
+              ))}
           </Menu>
           <RightMenu>
             <a href="#">Shop</a>
             <a href="#">Tesla Account</a>
-            <CustomMenu>
-              <MenuIcon />
-            </CustomMenu>
+            <CustomMenu onClick={() => setOpen(true) }/>
           </RightMenu>
+          <MenuNav show={open}>
+            <CloseWrapper>
+                <CustomClose onClick={() => setOpen(false)} />
+            </CloseWrapper>
+            {cars && cars.map((car, index) => (
+              <li key={index}><a href="#">{car}</a></li>
+            ))}
+            <li><a href="#">Existing Inventory</a></li>
+            <li><a href="#">Used Inventory</a></li>
+            <li><a href="#">Trade-in</a></li>
+            <li><a href="#">Cybertruck</a></li>
+            <li><a href="#">Roadster</a></li>
+            <li><a href="#">Semi</a></li>
+            <li><a href="#">Charging</a></li>
+            <li><a href="#">Power</a></li>
+            <li><a href="#">Commercial</a></li>
+            <li><a href="#">Utilities</a></li>
+            <li><a href="#">Test Drive</a></li>
+          </MenuNav>
         </Container>
     )
 }
@@ -38,7 +60,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
-  
+  z-index: 1;
 `;
 const Menu = styled.div`
   display: flex;
@@ -72,5 +94,42 @@ const RightMenu= styled.div`
 
 const CustomMenu= styled(MenuIcon)`
     cursor: pointer;
+`;
+
+const MenuNav =styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease-in;
+
+    li {
+      padding: 15px 0;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.23);
+
+      a {
+          font-weight: 600;
+      }
+    }
+`;
+
+const CloseWrapper= styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+
 
 `;
+
